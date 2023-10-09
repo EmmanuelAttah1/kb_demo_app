916d19fa0beb6aeb60f6c2a40fccfe8c7edfe2f6
Emmanuel <attahzuzu@gmail.com>
2023-10-09 15:12:58 +0100

    change websocket to production address

--- src/components/mainSection.js
+++ src/components/mainSection.js
@@ -75,7 +75,7 @@ export const MainSection = props =>{
 
     const chatSocket = new WebSocket(
         'ws://'
-        + 'localhost:8000'
+        + 'http://ec2-54-89-38-48.compute-1.amazonaws.com:8000'
         + '/ws/chat/'
     );
     
