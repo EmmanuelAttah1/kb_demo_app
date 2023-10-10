8571d6b59de1324ee43a28666c2ea1045fe85f7a
Emmanuel <attahzuzu@gmail.com>
2023-10-10 15:40:05 +0100

    change websocket api

--- src/components/mainSection.js
+++ src/components/mainSection.js
@@ -76,8 +76,9 @@ export const MainSection = props =>{
     const chatSocket = new WebSocket(
         'wss://'
         + 'api.getknowledgebase.com'
-        + '/ws/chat/'
     );
+
+    // + '/ws/chat/'
     
     chatSocket.onmessage = function(e) {
         setChatLoading(false)
